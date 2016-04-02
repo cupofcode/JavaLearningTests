de29254598897bc50e72229dc03bde9bac4ff48d
cupofcode <cupofcode@users.noreply.github.com>
2016-04-03 02:09:17 +0300

    Matcher find method is introduced.

--- learning_tests/test/learning/java/util/Matcher_FindTest.java
+++ learning_tests/test/learning/java/util/Matcher_FindTest.java
@@ -258,6 +258,295 @@ public class Matcher_FindTest {
 		assertMatcher("!", 0, 1);
 	}
 	
+	@Test
+	public void find_Quantifiers_ZeroLengthMatches() {
+		matcher = compile("a?").matcher(""); // once or not at all
+		assertTrue(matcher.find());
+		assertMatcher("", 0, 0);
+		
+		matcher = compile("a*").matcher(""); // zero or more times
+		assertTrue(matcher.find());
+		assertMatcher("", 0, 0);
+		
+		matcher = compile("a+").matcher(""); // one or more times
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_OneLengthMatches_OnceOrNotAtAll() {
+		matcher = compile("a?").matcher("a");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 1, 1);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_OneLengthMatches_ZeroOrMoreTimes() {
+		matcher = compile("a*").matcher("a");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 1, 1);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_OneLengthMatches_OneOrMoreTimes() {
+		matcher = compile("a+").matcher("a");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_TwoLengthMatches_OnceOrNotAtAll() {
+		matcher = compile("a?").matcher("aa");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 1, 2);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 2, 2);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_TwoLengthMatches_ZeroOrMoreTimes() {
+		matcher = compile("a*").matcher("aa");
+		
+		assertTrue(matcher.find());
+		assertMatcher("aa", 0, 2);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 2, 2);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_TwoLengthMatches_OneOrMoreTimes() {
+		matcher = compile("a+").matcher("aa");
+		
+		assertTrue(matcher.find());
+		assertMatcher("aa", 0, 2);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_AlternatingMatches_OnceOrNotAtAll() {
+		matcher = compile("a?").matcher("abbaab");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 1, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 2, 2);
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 3, 4);
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 4, 5);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 5, 5);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 6, 6);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_AlternatingMatches_ZeroOrMoreTimes() {
+		matcher = compile("a*").matcher("abbaab");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 1, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 2, 2);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aa", 3, 5);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 5, 5);
+		
+		assertTrue(matcher.find());
+		assertMatcher("", 6, 6);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_AlternatingMatches_OneOrMoreTimes() {
+		matcher = compile("a+").matcher("abbaab");
+		
+		assertTrue(matcher.find());
+		assertMatcher("a", 0, 1);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aa", 3, 5);
+		
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_Exactly_N_Times() {
+		matcher = compile("a{3}").matcher("aa");
+		assertFalse(matcher.find());
+		
+		matcher = compile("a{3}").matcher("aaa");
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 0, 3);
+		
+		matcher = compile("a{3}").matcher("aaaa");
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 0, 3);
+		
+		// 9 "a"s
+		matcher = compile("a{3}").matcher("aaaaaaaaaa");
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 0, 3);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 3, 6);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 6, 9);
+	}
+	
+	@Test
+	public void find_Quantifiers_AtLeast_N_Times() {
+		matcher = compile("a{3,}").matcher("aaaaaaaaa");
+		assertTrue(matcher.find());
+		assertMatcher("aaaaaaaaa", 0, 9);
+	}
+	
+	@Test
+	public void find_Quantifiers_AtLeast_N_TimesButNoMoreThan_M_Times() {
+		matcher = compile("a{3,6}").matcher("aaaaaaaaa");
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaaaaa", 0, 6);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 6, 9);
+	}
+	
+	@Test
+	public void find_Quantifiers_CapturingGroups() {
+		matcher = compile("(dog){3}").matcher("dogdogdogdogdogdogdogdogdog");
+		
+		assertTrue(matcher.find());
+		assertMatcher("dogdogdog", 0, 9);
+		
+		assertTrue(matcher.find());
+		assertMatcher("dogdogdog", 9, 18);
+	}
+	
+	@Test
+	public void find_Quantifiers_ApplyingToLastCharacter() {
+		matcher = compile("dog{3}").matcher("dogdogdogdogdogdogdogdogdog");
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_Quantifiers_ApplyingToCharacterClass() {
+		matcher = compile("[abc]{3}").matcher("abccabaaaccbbbc");
+		
+		assertTrue(matcher.find());
+		assertMatcher("abc", 0, 3);
+		
+		assertTrue(matcher.find());
+		assertMatcher("cab", 3, 6);
+		
+		assertTrue(matcher.find());
+		assertMatcher("aaa", 6, 9);
+		
+		assertTrue(matcher.find());
+		assertMatcher("ccb", 9, 12);
+		
+		assertTrue(matcher.find());
+		assertMatcher("bbc", 12, 15);
+	}
+	
+	@Test
+	public void find_Quantifiers_Greedy() {
+		matcher = compile(".*foo").matcher("xfooxxxxxxfoo");
+		
+		assertTrue(matcher.find());
+		assertMatcher("xfooxxxxxxfoo", 0, 13);
+	}
+	
+	@Test
+	public void find_Quantifiers_Relucant() {
+		matcher = compile(".*?foo").matcher("xfooxxxxxxfoo");
+		
+		assertTrue(matcher.find());
+		assertMatcher("xfoo", 0, 4);
+		
+		assertTrue(matcher.find());
+		assertMatcher("xxxxxxfoo", 4, 13);
+	}
+	
+	@Test
+	public void find_Quantifiers_Possessive() {
+		matcher = compile(".*+foo").matcher("xfooxxxxxxfoo");
+		assertFalse(matcher.find());
+	}
+	
+	@Test
+	public void find_CapturingGroups_Numbering() {
+		matcher = compile("((A)(B(C)))").matcher("ABC");
+		
+		assertTrue(matcher.find());
+		assertEquals(4, matcher.groupCount());
+		
+		assertEquals("ABC", matcher.group(0));
+		assertEquals(0, matcher.start(0));
+		assertEquals(3, matcher.end(0));
+		
+		assertEquals("ABC", matcher.group(1));
+		assertEquals(0, matcher.start(1));
+		assertEquals(3, matcher.end(1));
+		
+		assertEquals("A", matcher.group(2));
+		assertEquals(0, matcher.start(2));
+		assertEquals(1, matcher.end(2));
+		
+		assertEquals("BC", matcher.group(3));
+		assertEquals(1, matcher.start(3));
+		assertEquals(3, matcher.end(3));
+		
+		assertEquals("C", matcher.group(4));
+		assertEquals(2, matcher.start(4));
+		assertEquals(3, matcher.end(4));
+	}
+	
 	private void assertMatcher(String group, int start, int end) {
 		assertEquals(group, matcher.group());
 		assertEquals(start, matcher.start());
